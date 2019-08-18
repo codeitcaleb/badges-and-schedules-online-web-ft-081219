918fc06f9c4ce7c81582d1cb3acf5abde028d23c
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-18 16:34:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,37 +1,32 @@
+
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
+
 def batch_badge_creator(array)
   array_messages = []
   
-  array.each do |name| 
-    array_messages << badge_maker(name) 
-  end
+  array.each {|name| array_messages << badge_maker(name)}
   
   return array_messages
 end  
 
+
 def assign_rooms(array)
   room_assignment_messages = []
   
-  array.each_with_index do |name , index|
-    room_assignment_messages << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
-  end
+  array.each_with_index {|name , index| room_assignment_messages << "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
   
   return room_assignment_messages
 end  
+
 
 def printer(attendees)
   array_messages = batch_badge_creator(attendees)
   room_messages = assign_rooms(attendees)
   
-  array_messages.each do |message|
-   puts message
-  end
+  array_messages.each {|message| puts message}
   
-  room_messages.each do |message|
-    puts message 
-  end
-  
+  room_messages.each {|message| puts message}
 end  
